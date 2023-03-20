7a236af9fa0d4c0e344e61fb46e601a8f0436496
Ahmed Samy <ahmedov.samyvic@yahoo.com>
2023-03-20 21:10:01 +0200

    v1.2.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Samyvic';
+
+  phone = '';
+  phoneName = '';
+  
+  
+  getName(){
+    this.phoneName = this.phone;
+  }
+
+
+  
+    
+  
+
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'ng-4-44905',
+    appId: '1:44153938976:web:e640b242e9898033c28845',
+    storageBucket: 'ng-4-44905.appspot.com',
+    apiKey: 'AIzaSyASFwucb5ms4VSzFuOhQHYzNRyq3j1kQJU',
+    authDomain: 'ng-4-44905.firebaseapp.com',
+    messagingSenderId: '44153938976',
+    measurementId: 'G-XKTK6GK7EH',
+  },};
